ae8d93cf3ab9657d6dccde8780ce7b507ebdd43d
aestrada611 <>
2020-11-19 15:07:02 -0800

    Trying to fix git error

--- src/App.js
+++ src/App.js
@@ -6,14 +6,48 @@ class App extends Component{
   constructor(props){
     super(props)
     this.state = {
-      squares: [0, 0, 0, 0, 0, 0, 0, 0, 0]
+      squares: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      clickLocation: null
     }
   }
+
+//need a componentDidMount
+
+
 
 //we need to .map through this.state.squares and label their entire value,index, emoji
 // method to create the initial alert handleGamePlay
   handleGamePlay = (index) => {
-    alert("yo!" )
+
+  const { squares, clickLocation }
+    if(index === clickLocation){
+      squares[index] = "🌞"
+      this.setState({squares: squares})
+    }else if(index === clickLocation){
+      squares[index] = "🌚"
+      this.setState({squares: squares})
+    }
+//nest goal is to mark a square after it is clicked
+
+
+
+//
+// handleGamePlay = (index) => {
+//   const { squares, treasureLocation } = this.state
+//   if(index === treasureLocation){
+//     squares[index] = "🥑"
+// this.setState({ squares: squares })
+//   } else {
+//   squares[index] = "🌴"
+//   this.setState({ squares: squares })
+// }
+// }
+
+//different from treasure hunt becasue:
+//-This has two players, there is no treasure being placed within random square, There are multiple winning conditons within this game,
+
+
+
   }
 
   render(){
